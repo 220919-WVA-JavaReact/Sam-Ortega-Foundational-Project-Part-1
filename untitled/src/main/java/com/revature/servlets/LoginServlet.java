977978bed49f77c419bb365aeeda86784cc80a53
sam <ortegachan867@gmail.com>
2022-10-12 11:11:39 -0400

    html was not needed to commented that code out

---
+++ untitled/src/main/java/com/revature/servlets/LoginServlet.java
@@ -0,0 +1,25 @@
+package com.revature.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+//        resp.getWriter().write("<h1>test works!</h1>");
+//        resp.getWriter().write("<form action=\"login\" method=\"post\">");
+//        resp.getWriter().write("<label for=\"email\">Email:</label>");
+//        resp.getWriter().write("<input name=\"email\" size=\"30\" />");
+//        resp.getWriter().write("<br><br>");
+//        resp.getWriter().write("<label for=\"password\">Password:</label>");
+//        resp.getWriter().write("<input type=\"password\" name=\"password\" size=\"30\" />");
+//        resp.getWriter().write("<br><br>  ");
+//        resp.getWriter().write("<button type=\"submit\">Login</button>");
+//        resp.getWriter().write("</form>");
+    }
+}
